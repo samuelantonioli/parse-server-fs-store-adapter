54627a319e0bc3720a163960ed125fe6baacb340
Samuel Antonioli <contact@samuelantonioli.de>
2016-08-29 20:50:47 +0200

    added usage instructions, changed index.js for filesSubDirectory and removing bug in createFile

--- README.md
+++ README.md
@@ -2,12 +2,48 @@
 
 Use your file system for parse-server storage. Based on parse-server-fs-adapter
 
-After parse-server-fs-adapter didn't work for me i.e. getting errors like those:
+After parse-server-fs-adapter didn't work for me i.e. getting errors like this:
 
-https://github.com/ParsePlatform/parse-server/issues/1766  
 https://github.com/parse-server-modules/parse-server-fs-adapter/issues/3
 
 I read the source code of the Grid Store Adapter as an inspiration
 and used the functions of parse-server-fs-adapter.  
 
 Please read the source, it's short and it works.
+
+- - -
+
+Please create the `files` directory or the specified files directory **before** using it,  
+it doesn't create it automatically. Use e.g. `mkdir -p recursive/path/created/files`.  
+This saves probably error-prone code here.
+
+- - -
+
+**Usage**
+
+As an instance:
+
+    var FSStoreAdapter = require('parse-server-fs-store-adapter').FSStoreAdapter;
+    var fs_store_adapter = new FSStoreAdapter({
+        // caution: create this directory before you use it
+        filesSubDirectory: "other_files_directory" // optional, default: "files"
+    });
+
+    var api = new ParseServer({
+        // ...
+        filesAdapter: fs_store_adapter
+    });
+
+As a config option:
+
+    var api = new ParseServer({
+        // ...
+        filesAdapter: {
+            module: 'parse-server-fs-store-adapter',
+            options: {
+                // optional, default: 'files'
+                // caution: create this directory before you use it
+                filesSubDirectory: 'other_files_directory'
+            }
+        }
+    });
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 /**
  *
  * Using source code from parse-server-fs-adapter
- * After it didn't work for me i.e. getting errors like those:
+ * After it didn't work for me i.e. getting errors like this:
  *
- * https://github.com/ParsePlatform/parse-server/issues/1766
  * https://github.com/parse-server-modules/parse-server-fs-adapter/issues/3
  *
  * I read the source code of the Grid Store Adapter as an inspiration
@@ -16,67 +15,67 @@
  **/
 'use strict';
 
-var default_files_directory = 'files',
-    path = require('path'),
+var path = require('path'),
     fs = require('fs');
 
-var FSStoreAdapter = exports.FSStoreAdapter = function() {
-
-    function FSStoreAdapter() {
-        var files_directory = arguments.length <= 0 || arguments[0] === undefined ? default_files_directory : arguments[0];
-        this._files_directory = files_directory;
-        return this;
+function FSStoreAdapter(options) {
+    var files_directory = 'files';
+    if (typeof(options) === 'object' && typeof(options.filesSubDirectory) === 'string') {
+        files_directory = options.filesSubDirectory;
     }
+    this._files_directory = files_directory;
+    return this;
+}
 
-    FSStoreAdapter.prototype.buildpath = function(filename) {
-        return path.join(this._files_directory, encodeURIComponent(filename));
-    };
-    FSStoreAdapter.prototype.createFile = function(filename, data, contentType) {
-        return new Promise(function(resolve, reject) {
-            var filepath = this.buildpath(filename);
-            fs.writeFile(filepath, data, function(err) {
-                if(err !== null) {
-                    return reject(err);
+FSStoreAdapter.prototype.buildpath = function(filename) {
+    return path.join(this._files_directory, encodeURIComponent(filename));
+};
+FSStoreAdapter.prototype.createFile = function(filename, data, contentType) {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+        var filepath = self.buildpath(filename);
+        fs.writeFile(filepath, data, function(err) {
+            if (err !== null) {
+                return reject(err);
+            }
+            resolve(data);
+        });
+    });
+};
+FSStoreAdapter.prototype.deleteFile = function(filename) {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+        var filepath = self.buildpath(filename);
+        // TODO: data from readFile required?
+        fs.readFile(filepath, function(err, data) {
+            if (err !== null) {
+                return reject(err);
+            }
+            fs.unlink(filepath, function(err2) {
+                // error in original code, working here
+                if (err2 !== null) {
+                    return reject(err2);
                 }
                 resolve(data);
             });
         });
-    };
-    FSStoreAdapter.prototype.deleteFile = function(filename) {
-        var self = this;
-        return new Promise(function(resolve, reject) {
-            var filepath = self.buildpath(filename);
-            // TODO: data from readFile required?
-            fs.readFile(filepath, function(err, data) {
-                if(err !== null) {
-                    return reject(err);
-                }
-                fs.unlink(filepath, function(err2) {
-                    // error in original code, working here
-                    if(err2 !== null) {
-                        return reject(err2);
-                    }
-                    resolve(data);
-                });
-            });
+    });
+};
+FSStoreAdapter.prototype.getFileData = function(filename) {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+        var filepath = self.buildpath(filename);
+        fs.readFile(filepath, function (err, data) {
+            if (err !== null) {
+                return reject(err);
+            }
+            resolve(data);
         });
-    };
-    FSStoreAdapter.prototype.getFileData = function(filename) {
-        var self = this;
-        return new Promise(function(resolve, reject) {
-            var filepath = self.buildpath(filename);
-            fs.readFile(filepath, function (err, data) {
-                if(err !== null) {
-                    return reject(err);
-                }
-                resolve(data);
-            });
-        });
-    };
-    FSStoreAdapter.prototype.getFileLocation = function(config, filename) {
-        return config.mount + '/files/' + config.applicationId + '/' + encodeURIComponent(filename);
-    };
-    return FSStoreAdapter;
-}();
+    });
+};
+FSStoreAdapter.prototype.getFileLocation = function(config, filename) {
+    return config.mount + '/files/' + config.applicationId + '/' + encodeURIComponent(filename);
+};
 
+exports.FSStoreAdapter = FSStoreAdapter;
 exports.default = FSStoreAdapter;
